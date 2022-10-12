94603aa5d8f7993fa8f63f0c79e8dabc53092172
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2022-10-12 23:42:56 +0530

    Notification api

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ import EatingNav from './nav_components/EatingNav';
 import WeightNav from './nav_components/WeightNav';
 import WellnessNav from './nav_components/WellnessNav';
 import HealthyNav from './nav_components/HealthyNav';
-import BehindNav from './nav_components/BehindNav';
 import EngineeringNav from './nav_components/EngineeringNav';
 import NewBlog from './components/NewBlog';
 import Blog from './components/Blog';
 import Email from './nav_components/Email';
+import NavbarComponents from './nav_components/NavbarComponents';
+import banner8 from '../src/banner/banner8.jpg';
+
 
 function App() {
   return (
@@ -27,7 +29,7 @@ function App() {
         <Route path='/healthyeating' element={<EatingNav />} />
         <Route path='/healthymind' element={<HealthyNav />} />
         <Route path='/wellness' element={<WellnessNav />} />
-        <Route path='/behindscenes' element={<BehindNav />} />
+        <Route path='/behindscenes' element={<NavbarComponents category="Behind the scenes" img1={banner8} img2={"https://global-uploads.webflow.com/5fe33d036237252135e3e74d/61a5c5bd8fd76219dff5e445_1300x860.jpg"}/>} />
         <Route path='/healthyeating' element={<HealthyNav />} />
         <Route path='/engineering' element={<EngineeringNav />} />
         <Route path='/blog' element={<Blog/>} />
--- src/nav_components/Email.js
+++ src/nav_components/Email.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
 const Email = () => {
   return (
     <>
-      <div id='mail' className='flex justify-center border p-8 rounded-lg bg-gray-200'>
+      <div id='mail' className='flex justify-center bg-gray-200 p-8 rounded-lg'>
         <img className='w-1/4 rounded-tl-lg rounded-bl-lg' src="https://global-uploads.webflow.com/5fe33d02bcae60657c07a6a9/618e539ba1c7ab5d0e585e0d_Blog%20subscribe%20creative.webp" alt="" />
         <div className=' w-1/2'>
-        <p className='absolute right-60 text-gray-500/40 cursor-pointer'><Link to="/"><i class="fa-solid fa-xl fa-xmark"></i></Link></p>
+          <p className='absolute right-60 text-gray-500/40 cursor-pointer'><Link to="/"><i class="fa-solid fa-xl fa-xmark"></i></Link></p>
           <div className=' bg-white px-12 py-28 w-11/12 rounded-br-lg rounded-tr-lg'>
             <p className='text-center'>📬</p>
             <p className='font-bold text-2xl text-center'>Get The .fit Way Newsletter</p>
@@ -16,9 +16,9 @@ const Email = () => {
               <input className='border text-sm py-3 px-7 bg-num2 duration-300 rounded-lg hover:bg-white' type="text" name="" id="" placeholder='Email Address' />
               <button className="btn text-white font-bold rounded-lg bg-num1 px-5 py-3 mx-4 text-sm hover:bg-red-500">Subscribe</button>
             </div>
-            </div>
           </div>
         </div>
+      </div>
     </>
   )
 }
